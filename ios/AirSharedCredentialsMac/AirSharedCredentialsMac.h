15fc1b1f8a2d68739820f321456f2e59d907a11e
Mateo Kozomara <mateo.kozomara@gmail.com>
2021-01-26 19:31:09 +0100

    add new framework for 'sign in with apple' on macos

---
+++ ios/AirSharedCredentialsMac/AirSharedCredentialsMac.h
@@ -0,0 +1,19 @@
+//
+//  AirSharedCredentialsMac.h
+//  AirSharedCredentialsMac
+//
+//  Created by Mateo Kozomara on 26/01/2021.
+//  Copyright © 2021 FreshPlanet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirSharedCredentialsMac.
+FOUNDATION_EXPORT double AirSharedCredentialsMacVersionNumber;
+
+//! Project version string for AirSharedCredentialsMac.
+FOUNDATION_EXPORT const unsigned char AirSharedCredentialsMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirSharedCredentialsMac/PublicHeader.h>
+
+
